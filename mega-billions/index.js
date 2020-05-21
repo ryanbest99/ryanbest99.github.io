b78639516e6346c9c7852fc1f387a9a15289639d
ryanbest99 <49349102+ryanbest99@users.noreply.github.com>
2020-05-21 13:26:55 +0900

    Add files via upload

---
+++ mega-billions/index.js
@@ -0,0 +1,56 @@
+var randomNumber0 = Math.floor(Math.random()*70) + 1;
+var number0 = document.querySelectorAll(".drum")[0].innerHTML = randomNumber0;
+
+var randomNumber1 = Math.floor(Math.random()*70) + 1;
+var number1 = document.querySelectorAll(".drum")[1].innerHTML = randomNumber1;
+
+var randomNumber2 = Math.floor(Math.random()*70) + 1;
+var number2 = document.querySelectorAll(".drum")[2].innerHTML = randomNumber2;
+
+var randomNumber3 = Math.floor(Math.random()*70) + 1;
+var number3 = document.querySelectorAll(".drum")[3].innerHTML = randomNumber3;
+
+var randomNumber4 = Math.floor(Math.random()*70) + 1;
+var number4 = document.querySelectorAll(".drum")[4].innerHTML = randomNumber4;
+
+var randomNumber5 = Math.floor(Math.random()*26) + 1;
+var number5 = document.querySelectorAll(".drum")[5].innerHTML = randomNumber5;
+
+// if (randomNumber0 = randomNumber1) {
+    
+// }else if(randomNumber0 = randomNumber2) {
+    
+// }else if(randomNumber0 = randomNumber3) {
+    
+// }else if(randomNumber0 = randomNumber4) {
+    
+// }else if(randomNumber0 = randomNumber5) {
+    
+// }else if(randomNumber1 = randomNumber2) {
+    
+// }else if(randomNumber1 = randomNumber3) {
+    
+// }else if(randomNumber1 = randomNumber4) {
+    
+// }else if(randomNumber1 = randomNumber5) {
+    
+// }else if(randomNumber2 = randomNumber3) {
+    
+// }else if(randomNumber2 = randomNumber4) {
+    
+// }else if(randomNumber2 = randomNumber5) {
+    
+// }else if(randomNumber3 = randomNumber4) {
+    
+// }else if(randomNumber3 = randomNumber5) {
+    
+// }else if(randomNumber4 = randomNumber5) {
+    
+// }else {
+    
+// }
+
+
+
+
+
